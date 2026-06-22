ce67e168a10d6370b5b2922d4d2db9820f0ca5d2
alvinogthegreat177-stack <alvinogthegreat177@gmail.com>
2026-06-22 19:04:07 +0300

    app.py

--- app.py
+++ app.py
@@ -2252,13 +2252,7 @@ from flask import session, jsonify, request
 # USER SESSION CONFIGURATION
 # =========================================================
 
-from flask import session, jsonify, request
 
-app.config["SESSION_PERMANENT"] = True
-
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(
-    days=30
-)
 
 # =========================================================
 # API PROVIDERS
