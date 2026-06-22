02e1606d95c5aa875db0c52e3355e1f2b825992e
alvinogthegreat177-stack <alvinogthegreat177@gmail.com>
2026-06-22 20:11:51 +0300

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ import logging
 # ============================================================
 
 
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+
+app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
